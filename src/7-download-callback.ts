5092c6d6350b0341e0ea2736bdf9654cc4d46700
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-09-02 09:20:07 +0700

    create 7-download-callback.ts
    Created the download callback.

--- src/7-download-callback.ts
+++ src/7-download-callback.ts
@@ -1,16 +1,17 @@
-const downloadFiles = (message : string , url : string) => {
-  console.log(message + url)
-  
+type DownloadFilesFunction = (message : string , url : string) => void;
+
+const message  = "Downloading files from : "
+const url = "Website.com";
+
+const waitingForFileDownload : DownloadFilesFunction = (message, url) => {
   setTimeout(() => {
     console.log("Files downloaded!")
   }, 2000);
 }
 
-
-const message = "Downloading files from : "
-
-
-const url = "Website.com";
-
+const downloadFiles : DownloadFilesFunction = (message, url)=> {
+  console.log(message + url)
+  waitingForFileDownload(message , url);
+}
 
 downloadFiles(message , url);
