242ca969200a18a9b230fb5453b7b5429e7d889e
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-09-02 07:15:44 +0700

    Create 4-string-uppercase.ts
    Created the TypeScript code for the string to uppercase function.

---
+++ src/4-string-uppercase.ts
@@ -0,0 +1,19 @@
+const stringUppercase = () => {
+  
+  const stringBeforeUppercase : string = "Hello World!"
+  
+  console.log(`Your string before uppercase : ${stringBeforeUppercase}`)
+  
+  const stringAfterUppercase : string = stringBeforeUppercase.toUpperCase();
+
+  setTimeout(() : void => {
+    console.log(`Working on it...`)
+    setTimeout(() : void => {
+      console.log(`Your string in uppercase! : ${stringAfterUppercase}`)
+    }, 1000);
+  }, 3000);
+
+  
+}
+
+stringUppercase();
